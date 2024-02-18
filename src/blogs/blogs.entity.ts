f87b789bc328ce8e4d49273d825f09d35c751ad0
rimskayama <rimskayama@outlook.com>
2024-02-18 08:33:12 +0300

    blogs.entity.ts

---
+++ src/blogs/blogs.entity.ts
@@ -0,0 +1,44 @@
+import mongoose, { HydratedDocument } from 'mongoose'
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { BlogDto } from './blogs.types';
+import { ObjectId } from 'mongodb';
+
+export type BlogDocument = HydratedDocument<Blog>;
+
+@Schema()
+export class Blog {
+
+    @Prop({ 
+        required: true,
+        type: mongoose.Schema.Types.ObjectId 
+    })
+    _id: ObjectId;
+
+    @Prop({ required: true })
+    name: string;
+
+    @Prop({ required: true })
+    description: string;
+
+    @Prop({ required: true })
+    websiteUrl: string;
+
+    @Prop({ default: Date.now })
+    createdAt: string;
+
+    @Prop({ default: false })
+    isMembership: boolean;
+
+    static getViewBlog(blogFromDb: Blog): BlogDto {
+        return {
+            id: blogFromDb._id.toString(),
+            name: blogFromDb.name,
+            description: blogFromDb.description,
+            websiteUrl: blogFromDb.websiteUrl,
+            isMembership: blogFromDb.isMembership,
+            createdAt: blogFromDb.createdAt,
+        }
+    }
+}
+
+export const BlogSchema = SchemaFactory.createForClass(Blog);
