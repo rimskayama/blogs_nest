2aaaa90d5127dd7800721a1f42a50b0ca004bf89
rimskayama <rimskayama@outlook.com>
2024-02-18 08:33:28 +0300

    blogs.service.ts

---
+++ src/blogs/blogs.service.ts
@@ -0,0 +1,42 @@
+import { BlogInputDto, BlogDto } from "./blogs.types";
+import { ObjectId } from "mongodb";
+import { BlogsRepository } from "./blogs.repository";
+import { BlogsQueryRepository } from "./blogs.query.repository";
+import { Inject, Injectable } from "@nestjs/common";
+
+@Injectable()
+export class BlogsService {
+    constructor(
+        @Inject(BlogsRepository) protected blogsRepository: BlogsRepository,
+        @Inject(BlogsQueryRepository) protected blogsQueryRepository: BlogsQueryRepository) {
+    }
+
+    async createBlog(inputModel: BlogInputDto): Promise<BlogDto> {
+
+        const newBlog = {
+            _id: new ObjectId(),
+            name: inputModel.name,
+            description: inputModel.description,
+            websiteUrl: inputModel.websiteUrl,
+            createdAt: new Date().toString(),
+            isMembership: true
+        }
+
+        return await this.blogsRepository.createBlog(newBlog);
+    }
+
+    async updateBlog(id: string, inputModel: BlogInputDto):
+        Promise<BlogDto | boolean> {
+
+        return await this.blogsRepository.updateBlog(new ObjectId(id), inputModel);
+
+    }
+
+    async deleteBlog(id: string) {
+        return await this.blogsRepository.deleteBlog(new ObjectId(id));
+    }
+
+    async deleteAll() {
+        return await this.blogsRepository.deleteAll();
+    }
+}
