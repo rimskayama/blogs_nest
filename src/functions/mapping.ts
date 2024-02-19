f4a44b328718d6b6255fbce9402b5da1b32d5e21
rimskayama <rimskayama@outlook.com>
2024-02-19 06:48:50 +0300

    mapping.ts

---
+++ src/functions/mapping.ts
@@ -0,0 +1,20 @@
+import { Blog } from "src/blogs/blogs.entity"
+import { PostLike } from "src/likes/likes.entity"
+import { Post, likeDetails } from "src/posts/posts.entity"
+import { User } from "src/users/users.entity"
+
+export const blogsMapping = (array: Blog[]) => {
+    return array.map((b: Blog) => Blog.getViewBlog(b))
+}
+
+export const postsMapping = (array: Post[]) => {
+    return array.map((p: Post) => Post.getViewPost(p))
+}
+
+export const usersMapping = (array : User[]) => {
+    return array.map((u: User) => User.getViewUser(u))
+}
+
+export const likeDetailsMapping = (array: likeDetails[]) => {
+    return array.map((d: likeDetails) => PostLike.getViewLikeDetails(d))
+}
