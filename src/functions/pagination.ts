0ebfc53d6ba17b2fb7b5ac1781e08ee531da1511
rimskayama <rimskayama@outlook.com>
2024-02-20 10:59:39 +0300

    fixing

--- src/functions/pagination.ts
+++ src/functions/pagination.ts
@@ -6,7 +6,7 @@ export const getPagination = (query: QueryParameters) => {
         let limit: number = Number(query.pageSize) || 10;
         let sortDirection : Sort = query.sortDirection === 'asc' ? 1 : -1;
         let sortBy = query.sortBy || 'createdAt';
-        let sortByUsers = ('accountData.') + query.sortBy || 'createdAt';
+        let sortByUsers = 'accountData.' + query.sortBy || 'accountData.createdAt';
 
         let searchNameTerm = query.searchNameTerm || '';
         let searchLoginTerm = query.searchLoginTerm || '';
