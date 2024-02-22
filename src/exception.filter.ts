9a59f4e0490620ca001094733fdc3490ee907ad6
rimskayama <rimskayama@outlook.com>
2024-02-22 22:18:56 +0300

    exception.filter.ts

---
+++ src/exception.filter.ts
@@ -0,0 +1,26 @@
+import { ExceptionFilter, Catch, ArgumentsHost, HttpException } from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    const status = exception.getStatus();
+
+    if (status === 400) {
+        const errorResponse = {
+            errors: []
+        };
+        const responseBody: any = exception.getResponse();
+        responseBody.message.forEach((m) => errorResponse.errors.push(m));
+        response.status(status).json(errorResponse);
+    } else 
+    response
+    .json({
+        statusCode: status,
+        timestamp: new Date().toISOString(),
+        path: request.url,
+  })
+}}
