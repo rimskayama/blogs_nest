fb0aacf717eb0e509a9ef3d1526be58f660fb3c1
rimskayama <rimskayama@outlook.com>
2024-02-22 22:18:41 +0300

    added ValidationPipe

--- src/blogs/blogs.types.ts
+++ src/blogs/blogs.types.ts
@@ -1,7 +1,25 @@
-export type BlogInputDto = {
-    name: string, 
-    description: string,
-    websiteUrl: string,
+import { IsString, IsUrl, Length } from "class-validator";
+
+export class BlogInputDto {
+    @IsString({
+        message: "Incorrect type"})
+    @Length(1, 15, {
+        message: "Name length should be minimum 1 and maximum 500 symbols"
+    })
+    name: string;
+    @IsString({
+        message: "Incorrect type"})
+    @Length(1, 500, {
+        message: "Description length should be minimum 1 and maximum 15 symbols"})
+    description: string;
+    @IsString({
+        message: "Incorrect type"})
+    @Length(1, 100, {
+        message: "WebsiteUrl length should be minimum 1 and maximum 100 symbols"
+    })
+    @IsUrl({}, {
+        message: "Incorrect type"})
+    websiteUrl: string;
 }
 
 export type BlogDto = {
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.useGlobalPipes(new ValidationPipe({
+    stopAtFirstError: false,
+
+    exceptionFactory: (errors) => {
+      const errorsForResponse = [];
+      errors.forEach((e) => {
+        const constraintsKeys = Object.keys(e.constraints);
+        constraintsKeys.forEach(ckey => {
+          errorsForResponse.push({
+            message: e.constraints[ckey],
+            field: e.property
+          });
+        }); 
+      });
+
+      throw new BadRequestException(errorsForResponse)
+    },
+  })
+);
+  app.useGlobalFilters(new HttpExceptionFilter())
   await app.listen(5000);
 }
 bootstrap();
