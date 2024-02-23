26f6af4d71d5d5a8c0cf5816d0f2863f112edd55
rimskayama <rimskayama@outlook.com>
2024-02-23 20:30:25 +0300

    refactoring

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,37 +1,24 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { UsersController } from './users/users.controller';
-import { BlogsController } from './blogs/blogs.controller';
-import { BlogsService } from './blogs/blogs.service';
-import { BlogsRepository } from './blogs/blogs.repository';
 import { MongooseModule } from '@nestjs/mongoose';
-import { Blog } from './blogs/blogs.entity';
-import { BlogSchema } from './blogs/blogs.entity';
-import { BlogsQueryRepository } from './blogs/blogs.query.repository';
-import { Post, PostSchema } from './posts/posts.entity';
-import { User, UserSchema } from './users/users.entity';
-import { PostsController } from './posts/posts.controller';
-import { PostsService } from './posts/posts.service';
-import { PostsRepository } from './posts/posts.repository';
-import { PostsQueryRepository } from './posts/posts.query.repository';
-import { PostLike, PostLikeSchema } from './likes/likes.entity';
-import { UsersService } from './users/users.service';
-import { UsersRepository } from './users/users.repository';
-import { UsersQueryRepository } from './users/users.query.repository';
-import { TestingController } from './testing/testing.controller';
+import { BlogsModule } from './blogs/blogs.module';
+import { PostsModule } from './posts/posts.module';
+import { UsersModule } from './users/users.module';
+import { LikesModule } from './likes/likes.module';
+import { TestingModule } from './testing/testing.module';
+
 
 @Module({
   imports: [
 	ConfigModule.forRoot(),
 	MongooseModule.forRoot(process.env.MONGO_URL),
-	MongooseModule.forFeature([{ name: Blog.name, schema: BlogSchema }]),
-	MongooseModule.forFeature([{ name: Post.name, schema: PostSchema }]),
-	MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
-	MongooseModule.forFeature([{ name: PostLike.name, schema: PostLikeSchema }]),
+	BlogsModule,
+	PostsModule,
+	UsersModule, 
+	LikesModule,
+	TestingModule
   ],
-	controllers: [BlogsController, PostsController, UsersController, TestingController],
-	providers: [BlogsService, BlogsRepository, BlogsQueryRepository,
-				PostsService, PostsRepository, PostsQueryRepository,
-				UsersService, UsersRepository, UsersQueryRepository]
+	controllers: [],
+	providers: []
 })
 export class AppModule {}
