0588093c93d4d9e528ab887b583d9bd56912d6c8
rimskayama <rimskayama@outlook.com>
2024-02-24 09:14:48 +0300

    refactoring

--- src/filters/exception.filter.ts
+++ src/filters/exception.filter.ts
@@ -8,7 +8,7 @@ export class HttpExceptionFilter implements ExceptionFilter {
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
     const status = exception.getStatus();
-
+    
     if (status === 400) {
         const errorResponse = {
             errors: []
