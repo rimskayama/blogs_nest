406fa34ed10c3e6c42ad413766d1b33e89dd8f22
rimskayama <rimskayama@outlook.com>
2024-02-25 22:15:39 +0300

    validation

--- src/blogs/blogs.types.ts
+++ src/blogs/blogs.types.ts
@@ -1,42 +1,31 @@
-import { IsString, IsUrl, Length } from "class-validator";
+import { IsString, IsUrl, Length } from 'class-validator';
 
 export class BlogInputDto {
-    @IsString({
-        message: "Incorrect type"})
-    @Length(1, 15, {
-        message: "Name length should be minimum 1 and maximum 500 symbols"
-    })
-    name: string;
-    @IsString({
-        message: "Incorrect type"})
-    @Length(1, 500, {
-        message: "Description length should be minimum 1 and maximum 15 symbols"})
-    description: string;
-    @IsString({
-        message: "Incorrect type"})
-    @Length(1, 100, {
-        message: "WebsiteUrl length should be minimum 1 and maximum 100 symbols"
-    })
-    @IsUrl({}, {
-        message: "Incorrect type"})
-    websiteUrl: string;
+	@IsString()
+	@Length(1, 15)
+	name: string;
+	@IsString()
+	@Length(1, 500)
+	description: string;
+	@IsString()
+	@Length(1, 100)
+	@IsUrl()
+	websiteUrl: string;
 }
 
 export type BlogDto = {
-    id: string
-    name: string,
-    description: string,
-    websiteUrl: string,
-    isMembership: boolean | false,
-    createdAt: string
-}
+	id: string;
+	name: string;
+	description: string;
+	websiteUrl: string;
+	isMembership: boolean | false;
+	createdAt: string;
+};
 
-export type BlogsPaginationDto =
-    {
-        pagesCount: number;
-        page: number;
-        pageSize: number;
-        totalCount: number;
-        items: BlogDto[]
-    }
-
+export type BlogsPaginationDto = {
+	pagesCount: number;
+	page: number;
+	pageSize: number;
+	totalCount: number;
+	items: BlogDto[];
+};
