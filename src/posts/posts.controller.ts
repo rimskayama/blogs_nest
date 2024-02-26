edd460185caaf80fa0cbd3eed1d6ffea73ae3b41
rimskayama <rimskayama@outlook.com>
2024-02-26 22:21:16 +0300

    exception handling

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,78 +1,63 @@
-import { PostsService } from "./posts.service";
-import { PostsQueryRepository } from "./posts.query.repository";
-import { getPagination } from "../functions/pagination";
-import { PostInputDto } from "./posts.types";
-import { QueryParameters } from "src/users/users.types";
-import { 
-    Body,
-  Controller,  
-  Delete,  
-  Get, 
-  HttpCode, 
-  HttpException, 
-  HttpStatus, 
-  Inject, 
-  Param, 
-  Post, 
-  Put, 
-  Query
-} from '@nestjs/common';
+import { PostsService } from './posts.service';
+import { PostsQueryRepository } from './posts.query.repository';
+import { getPagination } from 'src/utils/pagination';
+import { PostInputDto } from './posts.types';
+import { QueryParameters } from 'src/users/users.types';
+import { Body, Controller, Delete, Get, HttpCode, Inject, Param, Post, Put, Query } from '@nestjs/common';
+import { exceptionHandler } from 'src/exceptions/exception.handler';
+import { StatusCode, blogIdField, blogNotFound, postIdField, postNotFound } from 'src/exceptions/exception.constants';
 
 @Controller('posts')
 export class PostsController {
-    constructor(
-        @Inject(PostsService) protected postsService: PostsService,
-        @Inject(PostsQueryRepository) protected postsQueryRepository: PostsQueryRepository
-        ) {}
-    
-    @Get()
-    @HttpCode(200)
-    async getPosts (@Query() query: QueryParameters) {
-    const {page, limit, sortDirection, sortBy, skip} = getPagination(query);
-    //authorization to get likeStatus
-    const allPosts = await this.postsQueryRepository.findPosts(
-        page, limit, sortDirection, sortBy, skip)
-        return allPosts
-    }
-    
-    @Get(':id')
-    @HttpCode(200)
-    async getPost (@Param('id') blogId: string) {
-        //authorization to get likeStatus
-        let post = await this.postsQueryRepository.findPostById(blogId);
-        if (post) return post
-        else throw new HttpException('Not Found', HttpStatus.NOT_FOUND);
-    }
+	constructor(
+		@Inject(PostsService) protected postsService: PostsService,
+		@Inject(PostsQueryRepository) protected postsQueryRepository: PostsQueryRepository
+	) {}
 
-    @Post()
-    @HttpCode(201)
-    async createPost (@Body() inputModel: PostInputDto) {
-        const newPost = await this.postsService.createPost(inputModel);
-        if (newPost) return newPost
-        else throw new HttpException('Not Found', HttpStatus.NOT_FOUND)
-    }
+	@Get()
+	@HttpCode(200)
+	async getPosts(@Query() query: QueryParameters) {
+		const { page, limit, sortDirection, sortBy, skip } = getPagination(query);
+		//authorization to get likeStatus
+		const result = await this.postsQueryRepository.findPosts(page, limit, sortDirection, sortBy, skip);
+		return result;
+	}
 
-    // getCommentsOfPost 
-    // createCommentByPostId 
+	@Get(':id')
+	@HttpCode(200)
+	async getPost(@Param('id') blogId: string) {
+		//authorization to get likeStatus
+		const result = await this.postsQueryRepository.findPostById(blogId);
+		if (result) return result;
+		else return exceptionHandler(StatusCode.NotFound, postNotFound, postIdField);
+	}
 
-    @Put(':id')
-    @HttpCode(204)
-    async updatePost (
-        @Body() inputModel: PostInputDto,
-        @Param('id') id: string 
-    ) {
-        const isUpdated = await this.postsService.updatePost(id, inputModel)
-        if (isUpdated) return
-        else throw new HttpException('Not Found', HttpStatus.NOT_FOUND)
-    }
+	@Post()
+	@HttpCode(201)
+	async createPost(@Body() inputModel: PostInputDto) {
+		const result = await this.postsService.createPost(inputModel);
+		if (result) return result;
+		else return exceptionHandler(StatusCode.NotFound, blogNotFound, blogIdField);
+	}
 
-    // updateLikeStatus 
+	// getCommentsOfPost
+	// createCommentByPostId
 
-    @Delete(':id')
-    @HttpCode(204)
-    async deletePost (@Param('id') postId: string) {
-        const result = await this.postsService.deletePost(postId);
-        if (result) return 
-        else throw new HttpException('Not Found', HttpStatus.NOT_FOUND);
-    }
+	@Put(':id')
+	@HttpCode(204)
+	async updatePost(@Body() inputModel: PostInputDto, @Param('id') id: string) {
+		const result = await this.postsService.updatePost(id, inputModel);
+		if (result) return;
+		else return exceptionHandler(StatusCode.NotFound, postNotFound, postIdField);
+	}
+
+	// updateLikeStatus
+
+	@Delete(':id')
+	@HttpCode(204)
+	async deletePost(@Param('id') postId: string) {
+		const result = await this.postsService.deletePost(postId);
+		if (result) return;
+		else return exceptionHandler(StatusCode.NotFound, postNotFound, postIdField);
+	}
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,58 +1,56 @@
-import { UsersService } from "./users.service";
-import { UsersQueryRepository } from "./users.query.repository";
-import { getPagination } from "../functions/pagination";
-import { ObjectId } from "mongodb";
-import { UserInputDto, QueryParameters } from "./users.types";
-import { 
-  Body,
-  Controller,  
-  Delete,  
-  Get, 
-  HttpCode, 
-  HttpException, 
-  HttpStatus, 
-  Inject, 
-  Param, 
-  Post, 
-  Query,
-} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UsersQueryRepository } from './users.query.repository';
+import { getPagination } from 'src/utils/pagination';
+import { ObjectId } from 'mongodb';
+import { UserInputDto, QueryParameters } from './users.types';
+import { Body, Controller, Delete, Get, HttpCode, Inject, Param, Post, Query } from '@nestjs/common';
+import { exceptionHandler } from 'src/exceptions/exception.handler';
+import { StatusCode, userIdField, userNotFound } from 'src/exceptions/exception.constants';
 
+//@UseGuards(AuthGuard)
 @Controller('users')
 export class UsersController {
-    constructor(@Inject(UsersService) protected usersService: UsersService,
-                @Inject(UsersQueryRepository) protected usersQueryRepository: UsersQueryRepository) {
-    }
+	constructor(
+		@Inject(UsersService) protected usersService: UsersService,
+		@Inject(UsersQueryRepository) protected usersQueryRepository: UsersQueryRepository
+	) {}
 
-    @Get()
-    @HttpCode(200)
-    async getUsers (@Query() query: QueryParameters) {
-    const {page, limit, sortDirection, sortByUsers, 
-        searchLoginTerm, searchEmailTerm, skip} = getPagination(query);
-    const allUsers = await this.usersQueryRepository.findUsers(
-        page, limit, sortDirection, sortByUsers, skip, searchLoginTerm, searchEmailTerm)
-        return allUsers
-    }
+	@Get()
+	@HttpCode(200)
+	async getUsers(@Query() query: QueryParameters) {
+		const { page, limit, sortDirection, sortByUsers, searchLoginTerm, searchEmailTerm, skip } = getPagination(query);
+		const result = await this.usersQueryRepository.findUsers(
+			page,
+			limit,
+			sortDirection,
+			sortByUsers,
+			skip,
+			searchLoginTerm,
+			searchEmailTerm
+		);
+		return result;
+	}
 
-    @Get(':id')
-    @HttpCode(200)
-    async getUser (@Param('id') userId: string) {
-        let user = await this.usersQueryRepository.findUserById(new ObjectId(userId))
-        if (user) return user
-        else throw new HttpException('Not Found', HttpStatus.NOT_FOUND)
-    }
+	@Get(':id')
+	@HttpCode(200)
+	async getUser(@Param('id') userId: string) {
+		const result = await this.usersQueryRepository.findUserById(new ObjectId(userId));
+		if (result) return result;
+		else return exceptionHandler(StatusCode.NotFound, userNotFound, userIdField);
+	}
 
-    @Post()
-    @HttpCode(201)
-    async createUser (@Body() inputModel: UserInputDto) {
-        const newUser = await this.usersService.createUser(inputModel)
-        return newUser
-    }
+	@Post()
+	@HttpCode(201)
+	async createUser(@Body() inputModel: UserInputDto) {
+		const result = await this.usersService.createUser(inputModel);
+		return result;
+	}
 
-    @Delete(':id')
-    @HttpCode(204)
-    async deleteUser (@Param('id') userId: string) {
-        const result = await this.usersService.deleteUser(userId);
-        if (result) return 
-        else throw new HttpException('Not Found', HttpStatus.NOT_FOUND);
-    }
+	@Delete(':id')
+	@HttpCode(204)
+	async deleteUser(@Param('id') userId: string) {
+		const result = await this.usersService.deleteUser(userId);
+		if (result) return;
+		else return exceptionHandler(StatusCode.NotFound, userNotFound, userIdField);
+	}
 }
