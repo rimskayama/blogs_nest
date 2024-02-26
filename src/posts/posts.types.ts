36b1af7dd9112ab627e8918daec05c63e8dc9075
rimskayama <rimskayama@outlook.com>
2024-02-26 22:18:28 +0300

    added validation

--- src/posts/posts.types.ts
+++ src/posts/posts.types.ts
@@ -1,33 +1,40 @@
-import { likeDetails } from "./posts.entity";
+import { IsString, Length } from 'class-validator';
+import { likeDetails } from './post.entity';
 
-export type PostInputDto = {
-    title: string,
-    shortDescription: string,
-    content: string,
-    blogId: string
+export class PostInputDto {
+	@IsString()
+	@Length(1, 30)
+	title: string;
+	@IsString()
+	@Length(1, 100)
+	shortDescription: string;
+	@IsString()
+	@Length(1, 1000)
+	content: string;
+	@IsString()
+	blogId: string;
 }
 
 export type PostDto = {
-    id: string;
-    title: string;
-    shortDescription: string;
-    content: string;
-    blogId: string;
-    blogName: string;
-    createdAt: string;
-    extendedLikesInfo: {
-        likesCount: number,
-        dislikesCount: number,
-        myStatus: string,
-        newestLikes: likeDetails[]
-    }
-}
+	id: string;
+	title: string;
+	shortDescription: string;
+	content: string;
+	blogId: string;
+	blogName: string;
+	createdAt: string;
+	extendedLikesInfo: {
+		likesCount: number;
+		dislikesCount: number;
+		myStatus: string;
+		newestLikes: likeDetails[];
+	};
+};
 
 export type postsPaginationDto = {
-    pagesCount: number;
-    page: number;
-    pageSize: number;
-    totalCount: number;
-    items: PostDto[]
-
-}
+	pagesCount: number;
+	page: number;
+	pageSize: number;
+	totalCount: number;
+	items: PostDto[];
+};
--- src/users/users.types.ts
+++ src/users/users.types.ts
@@ -1,34 +1,75 @@
-import { Sort } from "mongodb"
+import { IsEmail, IsString, Length, Matches, Validate } from 'class-validator';
+import { Sort } from 'mongodb';
+import {
+	confirmationCodeExistsRule,
+	emailConfirmedRule,
+	emailExistsRule,
+	loginExistsRule,
+	recoveryCodeExistsRule,
+} from 'src/auth/authentification';
+const loginPattern = /^[a-zA-Z0-9_-]*$/;
+
+export class UserInputDto {
+	@IsString()
+	@Length(3, 10)
+	@Matches(loginPattern, {
+		message: 'Login must be in correct format',
+	})
+	@Validate(loginExistsRule)
+	login: string;
+	@IsString()
+	@Length(6, 20)
+	password: string;
+	@IsString()
+	@IsEmail()
+	@Validate(emailExistsRule)
+	email: string;
+}
+
+export class newPasswordInputDto {
+	@IsString()
+	@Length(6, 20)
+	newPassword: string;
+	@IsString()
+	@Validate(recoveryCodeExistsRule)
+	recoveryCode: string;
+}
+
+export class emailInputDto {
+	@IsString()
+	@Validate(emailConfirmedRule)
+	email: string;
+}
+
+export class confirmationCodeInputDto {
+	@IsString()
+	@Validate(confirmationCodeExistsRule)
+	code: string;
+}
 
 export type UserDto = {
-    id: string,
-    login: string,
-    email: string,
-    createdAt: string
-}
-
-export type UserInputDto = {
-    login: string,
-    password: string,
-    email: string
-}
+	id: string;
+	login: string;
+	email: string;
+	createdAt: string;
+};
 
 export type UsersPaginationDto = {
-    pagesCount: number
-    page: number;
-    pageSize: number;
-    totalCount: number;
-    items: UserDto[]
-}
+	pagesCount: number;
+	page: number;
+	pageSize: number;
+	totalCount: number;
+	items: UserDto[];
+};
 
 export class QueryParameters {
-    pageNumber: number;
-    pageSize: number;
-    sortDirection: Sort;
-    sortBy: string;
-    sortByUsers: string;
-    searchNameTerm: string;
-    searchLoginTerm: string;
-    searchEmailTerm: string;
-    skip: number;
+	pageNumber: number;
+	pageSize: number;
+	sortDirection: Sort;
+	sortBy: string;
+	sortByUsers: string;
+	searchNameTerm: string;
+	searchLoginTerm: string;
+	searchEmailTerm: string;
+	skip: number;
 }
