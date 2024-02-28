05e0325520c278f68714ca66723fb23d2468c54e
rimskayama <rimskayama@outlook.com>
2024-02-28 22:14:17 +0300

    added imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,19 +6,20 @@ import { PostsModule } from './posts/posts.module';
 import { UsersModule } from './users/users.module';
 import { LikesModule } from './likes/likes.module';
 import { TestingModule } from './testing/testing.module';
-
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-  imports: [
-	ConfigModule.forRoot(),
-	MongooseModule.forRoot(process.env.MONGO_URL),
-	BlogsModule,
-	PostsModule,
-	UsersModule, 
-	LikesModule,
-	TestingModule
-  ],
-	controllers: [],
-	providers: []
+	imports: [
+		ConfigModule.forRoot(),
+		MongooseModule.forRoot(process.env.MONGO_URL),
+		BlogsModule,
+		PostsModule,
+		UsersModule,
+		LikesModule,
+		TestingModule,
+	],
+	controllers: [AppController],
+	providers: [AppService],
 })
 export class AppModule {}
