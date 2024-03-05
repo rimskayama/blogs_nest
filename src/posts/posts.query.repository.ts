9923c2ba975437d376f5490a5e881a05d9c75e7c
rimskayama <rimskayama@outlook.com>
2024-03-05 22:10:55 +0300

    fixed imports

--- src/posts/posts.query.repository.ts
+++ src/posts/posts.query.repository.ts
@@ -1,134 +1,160 @@
-import { PostDto, postsPaginationDto } from "./posts.types";
-import { ObjectId, SortDirection } from "mongodb";
-import { likeDetails } from './posts.entity';
-import { Post, PostDocument } from "./posts.entity";
-import { PostLike, PostLikeDocument } from "src/likes/likes.entity";
-import { InjectModel } from "@nestjs/mongoose";
+import { PostDto, postsPaginationDto } from './posts.types';
+import { ObjectId, SortDirection } from 'mongodb';
+import { likeDetails } from './post.entity';
+import { Post, PostDocument } from './post.entity';
+import { PostLike, PostLikeDocument } from '../likes/like.entity';
+import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { Injectable } from "@nestjs/common";
-import { postsMapping } from "../functions/mapping";
-
+import { Injectable } from '@nestjs/common';
+import { postsMapping } from '../utils/mapping';
 
 @Injectable()
 export class PostsQueryRepository {
-    constructor (
-        @InjectModel(Post.name)
-        private PostModel: Model<PostDocument>,
-        @InjectModel(PostLike.name)
-        private PostLikeModel: Model<PostLikeDocument>) {}
+	constructor(
+		@InjectModel(Post.name)
+		private PostModel: Model<PostDocument>,
+		@InjectModel(PostLike.name)
+		private PostLikeModel: Model<PostLikeDocument>
+	) {}
 
-    async findPosts(
-    page: number, limit: number, sortDirection: SortDirection,
-    sortBy: string, skip: number): Promise<postsPaginationDto> {
+	async findPosts(
+		page: number,
+		limit: number,
+		sortDirection: SortDirection,
+		sortBy: string,
+		skip: number
+	): Promise<postsPaginationDto> {
+		const allPosts = await this.PostModel.find({}, {})
+			.skip(skip)
+			.limit(limit)
+			.sort({ [sortBy]: sortDirection })
+			.lean();
 
-        let allPosts = await this.PostModel.find(
-            {},{})
-            .skip(skip)
-            .limit(limit)
-            .sort( {[sortBy]: sortDirection})
-            .lean()
+		const total = await this.PostModel.countDocuments();
 
-        const total = await this.PostModel.countDocuments()
+		const pagesCount = Math.ceil(total / limit);
 
-        const pagesCount = Math.ceil(total / limit)
+		const items = await Promise.all(
+			allPosts.map(async (post) => {
+				const likeStatus = 'None';
+				//    if (userId) {
+				//        const likeInDB = await this.PostLikeModel.findOne(
+				//            {postId: post._id.toString()})
+				//        if (likeInDB) {
+				//            likeStatus = likeInDB.status
+				//        }
+				//    }
 
-       const items = await Promise.all(allPosts.map(async (post) => {
-           let likeStatus = "None"
-        //    if (userId) {
-        //        const likeInDB = await this.PostLikeModel.findOne(
-        //            {postId: post._id.toString()})
-        //        if (likeInDB) {
-        //            likeStatus = likeInDB.status
-        //        }
-        //    }
+				const newestLikes: likeDetails[] = await this.PostLikeModel.find({
+					postId: post._id.toString(),
+					status: 'Like',
+				})
+					.sort({ addedAt: -1 })
+					.limit(3)
+					.lean();
+				return Post.getViewPost({
+					...post,
+					extendedLikesInfo: {
+						...post.extendedLikesInfo,
+						newestLikes,
+						myStatus: likeStatus,
+					},
+				});
+			})
+		);
+		return {
+			pagesCount: pagesCount,
+			page: page,
+			pageSize: limit,
+			totalCount: total,
+			items,
+		};
+	}
 
-           const newestLikes: likeDetails[] = await this.PostLikeModel
-               .find({ postId: post._id.toString(), status: "Like"})
-               .sort ({addedAt: -1})
-               .limit (3)
-               .lean()
-           return Post.getViewPost({...post, extendedLikesInfo: {
-                   ...post.extendedLikesInfo, newestLikes, myStatus: likeStatus
-               }})
+	async findPostById(id: string): Promise<PostDto | null> {
+		const post: Post | null = await this.PostModel.findOne({ _id: new ObjectId(id) }).lean();
+		if (!post) {
+			return null;
+		}
 
-       }))
-        return {
-            pagesCount: pagesCount,
-            page: page,
-            pageSize: limit,
-            totalCount: total,
-            items
-        }
-    }
+		const myStatus = 'None';
+		// if (userId) {
+		//     const likeInDB = await this.PostLikeModel.findOne(
+		//         {postId: post._id.toString(), userId: userId})
+		//     if (likeInDB) {
+		//         myStatus = likeInDB.status
+		//     }
+		// }
 
-    async findPostById(id: string): Promise<PostDto | null> {
-        const post: Post | null = await this.PostModel.findOne({_id: new ObjectId(id)}).lean();
-        if (!post) {
-            return null;
-        }
+		const newestLikes: likeDetails[] = await this.PostLikeModel.find({ postId: post._id.toString(), status: 'Like' })
+			.sort({ addedAt: -1 })
+			.limit(3)
+			.lean();
 
-        let myStatus = "None"
-        // if (userId) {
-        //     const likeInDB = await this.PostLikeModel.findOne(
-        //         {postId: post._id.toString(), userId: userId})
-        //     if (likeInDB) {
-        //         myStatus = likeInDB.status
-        //     }
-        // }
+		return Post.getViewPost({
+			...post,
+			extendedLikesInfo: {
+				...post.extendedLikesInfo,
+				newestLikes,
+				myStatus,
+			},
+		});
+	}
 
-        const newestLikes: likeDetails[] = await this.PostLikeModel
-            .find({ postId: post._id.toString(), status: "Like"},)
-            .sort ({addedAt: -1})
-            .limit (3)
-            .lean()
+	async findPostsByBlogId(
+		blogId: string,
+		page: number,
+		limit: number,
+		sortDirection: SortDirection,
+		sortBy: string,
+		skip: number
+	) {
+		const postsByBlogId = await this.PostModel.find({ blogId: blogId })
+			.skip(skip)
+			.limit(limit)
+			.sort({ [sortBy]: sortDirection })
+			.lean();
 
-        return Post.getViewPost({...post, extendedLikesInfo:{
-            ...post.extendedLikesInfo, newestLikes, myStatus
-            }})
-    }
+		const total = await this.PostModel.countDocuments({ blogId: blogId });
 
-    async findPostsByBlogId(blogId: string, page: number, limit: number, sortDirection: SortDirection,
-                            sortBy: string, skip: number) {
-        const postsByBlogId = await this.PostModel.find(
-            {blogId: blogId},
-            )
-            .skip(skip)
-            .limit(limit)
-            .sort( {[sortBy]: sortDirection})
-            .lean()
+		const pagesCount = Math.ceil(total / limit);
 
-        const total = await this.PostModel.countDocuments({blogId: blogId})
+		await Promise.all(
+			postsByBlogId.map(async (post) => {
+				const likeStatus = 'None';
+				// if (userId) {
+				//     const likeInDB = await this.PostLikeModel.findOne(
+				//         {postId: post._id.toString(), userId: userId})
+				//     if (likeInDB) {
+				//         likeStatus = likeInDB.status
+				//     }
+				// }
 
-        const pagesCount = Math.ceil(total / limit)
+				const newestLikes: likeDetails[] = await this.PostLikeModel.find({
+					postId: post._id.toString(),
+					status: 'Like',
+				})
+					.sort({ addedAt: -1 })
+					.limit(3)
+					.lean();
 
-        const items = await Promise.all(postsByBlogId.map(async(post) => {
-            let likeStatus = "None"
-            // if (userId) {
-            //     const likeInDB = await this.PostLikeModel.findOne(
-            //         {postId: post._id.toString(), userId: userId})
-            //     if (likeInDB) {
-            //         likeStatus = likeInDB.status
-            //     }
-            // }
+				return Post.getViewPost({
+					...post,
+					extendedLikesInfo: {
+						...post.extendedLikesInfo,
+						newestLikes,
+						myStatus: likeStatus,
+					},
+				});
+			})
+		);
 
-            const newestLikes: likeDetails[] = await this.PostLikeModel
-                .find({ postId: post._id.toString(), status: "Like"},)
-                .sort ({addedAt: -1})
-                .limit (3)
-                .lean()
-
-            return Post.getViewPost({...post, extendedLikesInfo:{
-                    ...post.extendedLikesInfo, newestLikes, myStatus: likeStatus
-                }})
-
-        }))
-
-        return {
-            pagesCount: pagesCount,
-            page: page,
-            pageSize: limit,
-            totalCount: total,
-            items: postsMapping(postsByBlogId)
-        }
-}
+		return {
+			pagesCount: pagesCount,
+			page: page,
+			pageSize: limit,
+			totalCount: total,
+			items: postsMapping(postsByBlogId),
+		};
+	}
 }
