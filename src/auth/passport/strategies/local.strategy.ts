1b1b6e7e8f505e858e233d3f274bd19bb444da45
rimskayama <rimskayama@outlook.com>
2024-03-09 21:16:52 +0300

    local.strategy.ts

---
+++ src/auth/passport/strategies/local.strategy.ts
@@ -0,0 +1,24 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { AuthService } from '../../auth.service';
+import { Strategy } from 'passport-local';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { User } from 'src/users/user.entity';
+import { jwtConstants } from '../../constants';
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+	constructor(private readonly authService: AuthService) {
+		super({
+			usernameField: 'loginOrEmail',
+			secretOrKey: jwtConstants.accessTokenSecret,
+		});
+	}
+	async validate(loginOrEmail: string, password: string): Promise<UnauthorizedException | User> {
+		const user = await this.authService.validateUser(loginOrEmail, password);
+		if (!user) {
+			throw new UnauthorizedException();
+		}
+
+		return user;
+	}
+}
