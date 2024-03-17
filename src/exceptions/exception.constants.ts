9419d525a109e134233d73bf7fe5e251864dbcc2
rimskayama <rimskayama@outlook.com>
2024-03-17 20:30:54 +0300

    added constants for devices

--- src/exceptions/exception.constants.ts
+++ src/exceptions/exception.constants.ts
@@ -2,11 +2,17 @@
 export const blogIdField = 'blogId';
 export const postIdField = 'postId';
 export const userIdField = 'userId';
+export const deviceIdField = 'deviceId';
 
 // Not found
 export const blogNotFound = 'Blog not found';
 export const postNotFound = 'Post not found';
 export const userNotFound = 'User not found';
+export const deviceNotFound = 'Device not found';
+
+// Forbidden
+
+export const forbidden = 'Forbidden';
 
 export enum StatusCode {
 	Success,
