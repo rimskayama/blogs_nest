714e94c35edfe30e896371456c0d8352548bb4b0
rimskayama <rimskayama@outlook.com>
2024-03-17 20:28:33 +0300

    devices.types.ts

---
+++ src/devices/devices.types.ts
@@ -0,0 +1,13 @@
+export type DeviceViewDto = {
+	ip: string;
+	title: string;
+	lastActiveDate: string;
+	deviceId: string;
+};
+
+export type DeviceDto = {
+	ip: string;
+	title: string;
+	lastActiveDate: number;
+	deviceId: string;
+};
