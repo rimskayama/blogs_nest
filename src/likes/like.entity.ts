1e2f1684431802a2999899263d1b8a9e93fcc29d
rimskayama <rimskayama@outlook.com>
2024-03-18 13:51:36 +0300

    added comment likes

--- src/likes/like.entity.ts
+++ src/likes/like.entity.ts
@@ -1,12 +1,19 @@
-import { HydratedDocument } from 'mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { likeDetails } from 'src/posts/post.entity';
 import { likeDetailsDto } from './likes.types';
+import { ObjectId } from 'mongodb';
 
 export type PostLikeDocument = HydratedDocument<PostLike>;
 
 @Schema()
 export class PostLike {
+	@Prop({
+		required: true,
+		type: mongoose.Schema.Types.ObjectId,
+	})
+	_id: ObjectId;
+
 	@Prop({ required: true })
 	postId: string;
 
@@ -31,3 +38,31 @@ export class PostLike {
 	}
 }
 export const PostLikeSchema = SchemaFactory.createForClass(PostLike);
+
+export type CommentLikeDocument = HydratedDocument<CommentLike>;
+
+@Schema()
+export class CommentLike {
+	@Prop({
+		required: true,
+		type: mongoose.Schema.Types.ObjectId,
+	})
+	_id: ObjectId;
+
+	@Prop({ required: true })
+	commentId: string;
+
+	@Prop({ required: true })
+	status: string;
+
+	@Prop({ required: true })
+	userId: string;
+
+	@Prop({ required: true })
+	userLogin: string;
+
+	@Prop({ required: true })
+	addedAt: string;
+}
+
+export const CommentLikeSchema = SchemaFactory.createForClass(CommentLike);
