55f7646ec924fbc919c35181e9b3589fa979ed40
rimskayama <rimskayama@outlook.com>
2024-03-20 17:02:25 +0300

    added devices mapping

--- src/utils/mapping.ts
+++ src/utils/mapping.ts
@@ -1,7 +1,9 @@
 import { Blog } from '../blogs/blog.entity';
+import { Device } from '../devices/device.entity';
 import { PostLike } from '../likes/like.entity';
 import { Post, likeDetails } from '../posts/post.entity';
 import { User } from '../users/user.entity';
+import { Comment } from '../comments/comment.entity';
 
 export const blogsMapping = (array: Blog[]) => {
 	return array.map((b: Blog) => Blog.getViewBlog(b));
@@ -13,6 +15,10 @@ export const postsMapping = (array: Post[]) => {
 
 export const usersMapping = (array: User[]) => {
 	return array.map((u: User) => User.getViewUser(u));
+};
+
+export const devicesMapping = (array: Device[]) => {
+	return array.map((d: Device) => Device.getViewDevice(d));
 };
 
 export const likeDetailsMapping = (array: likeDetails[]) => {
