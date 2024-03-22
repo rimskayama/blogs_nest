1d3321d12140f5c904ac8c82787dd73aab269874
rimskayama <rimskayama@outlook.com>
2024-03-22 09:48:02 +0300

    imports

--- src/likes/likes.module.ts
+++ src/likes/likes.module.ts
@@ -1,15 +1,23 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { PostLike, PostLikeSchema } from './like.entity';
+import { CommentLike, CommentLikeSchema, PostLike, PostLikeSchema } from './like.entity';
+import { LikesService } from './likes.service';
+import { PostLikesRepository } from './post.likes.repository';
+import { CommentLikesRepository } from './comment.likes.repository';
+import { UsersQueryRepository } from '../users/users.query.repository';
+import { User, UserSchema } from '../users/user.entity';
+import { JwtBearerStrategy } from '../auth/passport/strategies/jwt-bearer.strategy';
 
 @Module({
 	imports: [
 		ConfigModule.forRoot(),
 		MongooseModule.forRoot(process.env.MONGO_URL),
+		MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
 		MongooseModule.forFeature([{ name: PostLike.name, schema: PostLikeSchema }]),
+		MongooseModule.forFeature([{ name: CommentLike.name, schema: CommentLikeSchema }]),
 	],
 	controllers: [],
-	providers: [],
+	providers: [LikesService, PostLikesRepository, CommentLikesRepository, UsersQueryRepository, JwtBearerStrategy],
 })
 export class LikesModule {}
