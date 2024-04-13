3a92e373d4aada85331b480fadf9fed21a313bf5
rimskayama <rimskayama@outlook.com>
2024-04-13 17:58:36 +0300

    refactoring

--- src/testing/testing.controller.ts
+++ src/testing/testing.controller.ts
@@ -1,14 +1,14 @@
 import { BlogsService } from '../blogs/blogs.service';
 import { PostsService } from '../posts/posts.service';
 import { UsersService } from '../users/users.service';
-import { Controller, Delete, HttpCode, Inject } from '@nestjs/common';
+import { Controller, Delete, HttpCode } from '@nestjs/common';
 
 @Controller('testing')
 export class TestingController {
 	constructor(
-		@Inject(BlogsService) protected blogsService: BlogsService,
-		@Inject(PostsService) protected postsService: PostsService,
-		@Inject(UsersService) protected usersService: UsersService
+		private readonly blogsService: BlogsService,
+		private readonly postsService: PostsService,
+		private readonly usersService: UsersService
 	) {}
 
 	@Delete('all-data')
