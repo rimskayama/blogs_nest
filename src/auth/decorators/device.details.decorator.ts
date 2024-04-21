d784cef829c2c52d84b7e46895cbb14ccad3789e
rimskayama <rimskayama@outlook.com>
2024-04-21 10:11:19 +0300

    types fixing

--- src/auth/decorators/device.details.decorator.ts
+++ src/auth/decorators/device.details.decorator.ts
@@ -5,11 +5,9 @@ export const DeviceDetais = createParamDecorator((data: unknown, ctx: ExecutionC
 	try {
 		const deviceName = request.headers['user-agent'] || 'Device name';
 		const ip = request.socket.remoteAddress || 'IP address';
-		const expDate = request.headers.expires || 'expDate';
 		return {
 			deviceName,
 			ip,
-			expDate,
 		};
 	} catch (e) {
 		return false;
--- src/devices/devices.types.ts
+++ src/devices/devices.types.ts
@@ -11,3 +11,20 @@ export type DeviceDto = {
 	lastActiveDate: number;
 	deviceId: string;
 };
+
+export class DeviceType {
+	userId: string;
+	ip: string;
+	title: string;
+	lastActiveDate: number;
+	deviceId: string;
+	tokenExpirationDate: number;
+	static getViewDevice(deviceFromDb: DeviceType): DeviceViewDto {
+		return {
+			ip: deviceFromDb.ip,
+			title: deviceFromDb.title,
+			lastActiveDate: new Date(deviceFromDb.lastActiveDate * 1000).toISOString(),
+			deviceId: deviceFromDb.deviceId,
+		};
+	}
+}
