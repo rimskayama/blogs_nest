2b40628046924f1bbcc91b41d64a98d6264df745
rimskayama <rimskayama@outlook.com>
2024-04-24 10:18:33 +0300

    types fixing

--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -1,4 +1,4 @@
-import { ObjectId } from 'mongodb';
+import { v4 as uuidv4 } from 'uuid';
 import { UsersQueryRepository } from '../users/users.query.repository';
 import { CommentsRepository } from './comments.repository';
 import { CommentsQueryRepository } from './comments.query.repository';
@@ -14,41 +14,28 @@ export class CommentsService {
 		@Inject(UsersQueryRepository) protected usersQueryRepository: UsersQueryRepository
 	) {}
 
-	async findCommentById(id: string, userId: string | false) {
-		return await this.commentsRepository.findCommentById(new ObjectId(id), userId);
-	}
-
 	async createComment(content: string, userId: string, postId: string) {
-		const foundUserById = await this.usersQueryRepository.findUserById(new ObjectId(userId));
+		const foundUserById = await this.usersQueryRepository.findUserById(userId);
 		if (foundUserById) {
 			const newComment = {
-				_id: new ObjectId(),
+				id: uuidv4(),
 				postId: postId,
 				content: content,
-				createdAt: new Date().toISOString(),
-				commentatorInfo: {
-					userId: foundUserById.id,
-					userLogin: foundUserById.login,
-				},
-				likesInfo: {
-					likesCount: 0,
-					dislikesCount: 0,
-					myStatus: 'None',
-				},
+				createdAt: new Date(),
+				commentatorId: foundUserById.id,
+				commentatorLogin: foundUserById.login,
+				likesCount: 0,
+				dislikesCount: 0,
 			};
 			return await this.commentsRepository.createComment(newComment);
 		} else return null;
 	}
 
 	async updateComment(id: string, content: string) {
-		return await this.commentsRepository.updateComment(new ObjectId(id), content);
+		return await this.commentsRepository.updateComment(id, content);
 	}
 
 	async deleteComment(id: string) {
-		return await this.commentsRepository.deleteComment(new ObjectId(id));
-	}
-
-	async deleteAll() {
-		return await this.commentsRepository.deleteAll();
+		return await this.commentsRepository.deleteComment(id);
 	}
 }
