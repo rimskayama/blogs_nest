c75b14f662d5b51b67bfa632b486434b83ef4154
rimskayama <rimskayama@outlook.com>
2024-04-24 10:18:24 +0300

    comment likes migration to Sql

--- src/likes/comment.likes.repository.ts
+++ src/likes/comment.likes.repository.ts
@@ -1,68 +1,108 @@
 import { Injectable } from '@nestjs/common';
-import { CommentLike, CommentLikeDocument } from './like.entity';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { InjectDataSource } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import { CommentLikeType, LikeStatus } from './likes.types';
 
 @Injectable()
 export class CommentLikesRepository {
-	constructor(@InjectModel(CommentLike.name) private commentLikeModel: Model<CommentLikeDocument>) {}
+	constructor(@InjectDataSource() protected dataSource: DataSource) {}
 	async countLikes(commentId: string) {
-		const likesCount = await this.commentLikeModel.countDocuments({
-			status: 'Like',
-			commentId: commentId,
-		});
-		const dislikesCount = await this.commentLikeModel.countDocuments({
-			status: 'Dislike',
-			commentId: commentId,
-		});
+		let likes;
+		let dislikes;
+		const queryToCountLikes = `
+		SELECT count(*) as total
+		FROM public."CommentLikes" cl
+		WHERE cl."commentId" = $1 AND cl."status" = $2;`;
+
+		try {
+			likes = await this.dataSource.query(queryToCountLikes, [commentId, LikeStatus.Like]);
+			dislikes = await this.dataSource.query(queryToCountLikes, [commentId, LikeStatus.Dislike]);
+		} catch (error) {
+			console.error('Error counting comment likes', error);
+		}
+
+		const likesCount = parseInt(likes[0].total);
+		const dislikesCount = parseInt(dislikes[0].total);
+
 		return {
 			likesCount: likesCount,
 			dislikesCount: dislikesCount,
 		};
 	}
 
-	async setLikeStatus(newStatus: CommentLike) {
-		const commentId = newStatus.commentId;
-		const userId = newStatus.userId;
-		await this.commentLikeModel.create(newStatus);
-		const like = await this.commentLikeModel.findOne({ $and: [{ commentId: commentId }, { userId: userId }] });
-		if (like) {
+	async setLikeStatus(newStatus: CommentLikeType) {
+		const query = `INSERT INTO public."CommentLikes"(
+				"id", "commentId", "status", "userId", "login", "addedAt")
+			VALUES($1, $2, $3, $4, $5, $6)
+			RETURNING *
+			`;
+
+		const values = [
+			newStatus.id,
+			newStatus.commentId,
+			newStatus.status,
+			newStatus.userId,
+			newStatus.login,
+			newStatus.addedAt,
+		];
+
+		try {
+			const result = await this.dataSource.query(query, values);
+			if (result[1] === 0) {
+				return false;
+			}
 			return true;
+		} catch (error) {
+			console.error('Error creating comment like status :', error);
+			return false;
 		}
-		return false;
 	}
 
 	async checkLikeInDB(commentId: string, userId: string) {
-		const like = await this.commentLikeModel.findOne({ $and: [{ commentId: commentId }, { userId: userId }] });
-		if (like) {
-			return like;
+		const query = `
+        SELECT "status"
+		FROM public."CommentLikes" cl
+		WHERE cl."commentId" = $1 AND cl."userId" = $2;
+    `;
+
+		try {
+			const result = await this.dataSource.query(query, [commentId, userId]);
+			return result[0];
+		} catch (error) {
+			console.error('Error finding comment like status:', error);
+			return null;
 		}
-		return false;
 	}
 
 	async updateLikeStatus(likeStatus: string, commentId: string, userId: string) {
-		return this.commentLikeModel.findOneAndUpdate(
-			{ $and: [{ commentId: commentId }, { userId: userId }] },
-			{
-				$set: {
-					status: likeStatus,
-					addedAt: new Date().toISOString(),
-				},
-			}
-		);
+		const query = `
+			UPDATE public."CommentLikes" cl
+			SET "status"=$1, "addedAt"=$2
+			WHERE cl."commentId" = $3 AND cl."userId" = $4;
+		`;
+		const values = [likeStatus, new Date(), commentId, userId];
+
+		try {
+			return await this.dataSource.query(query, values);
+		} catch (error) {
+			console.error('Error updating comment like status:', error);
+		}
 	}
 
 	async removeLike(commentId: string, userId: string) {
-		await this.commentLikeModel.deleteOne({ $and: [{ commentId: commentId }, { userId: userId }] });
-		const like = await this.commentLikeModel.findOne({ $and: [{ commentId: commentId }, { userId: userId }] });
-		if (!like) {
+		const query = `
+		DELETE FROM public."CommentLikes" cl
+        WHERE cl."commentId" = $1 AND cl."userId" = $2;
+	`;
+		try {
+			const result = await this.dataSource.query(query, [commentId, userId]);
+			if (result[1] === 0) {
+				return null;
+			}
 			return true;
+		} catch (error) {
+			console.error('Error deleting comment like status:', error);
+			return null;
 		}
-		return false;
 	}
-
-	// async getCommentLikes(commentId: string) {
-	//     const commentLikes = await LikeModel.find({commentId: commentId}).lean()
-	//     return {commentLikes}
-	// }
 }
--- src/likes/likes.service.ts
+++ src/likes/likes.service.ts
@@ -1,6 +1,7 @@
 import { UsersQueryRepository } from '../users/users.query.repository';
 import { PostLikesRepository } from './post.likes.repository';
 import { Inject, Injectable } from '@nestjs/common';
+import { CommentViewDto } from '../comments/comments.types';
 import { CommentLikesRepository } from './comment.likes.repository';
 import { PostViewDto } from '../posts/posts.types';
 import { v4 as uuidv4 } from 'uuid';
@@ -12,6 +13,43 @@ export class LikesService {
 		@Inject(UsersQueryRepository) protected usersQueryRepository: UsersQueryRepository,
 		@Inject(CommentLikesRepository) protected commentLikesRepository: CommentLikesRepository
 	) {}
+
+	//COMMENT LIKES
+
+	async setCommentLikeStatus(likeStatus: string, comment: CommentViewDto, userId: string) {
+		const user = await this.usersQueryRepository.findUserById(userId);
+		const like = {
+			id: uuidv4(),
+			commentId: comment.id,
+			status: likeStatus,
+			userId: userId,
+			login: user!.login,
+			addedAt: new Date(),
+		};
+		return await this.commentLikesRepository.setLikeStatus(like);
+	}
+
+	async checkCommentLikeStatus(likeStatus: string, commentId: string, userId: string) {
+		if (likeStatus === 'None') {
+			await this.commentLikesRepository.removeLike(commentId, userId);
+			return true;
+		} else {
+			//Like or Dislike
+			const likeInDB = await this.commentLikesRepository.checkLikeInDB(commentId, userId);
+			if (!likeInDB) return false; // can be created
+			if (likeInDB) {
+				if (likeInDB.status === likeStatus) return true;
+				if (likeInDB.status !== likeStatus) {
+					await this.commentLikesRepository.updateLikeStatus(likeStatus, commentId, userId);
+					return true;
+				}
+			}
+		}
+	}
+
+	async countCommentLikes(commentId: string) {
+		return await this.commentLikesRepository.countLikes(commentId);
+	}
 
 	// POST LIKES
 
