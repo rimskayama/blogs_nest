14379e4b10a94e04645df912f54df281f84ee24a
rimskayama <rimskayama@outlook.com>
2024-04-26 17:04:51 +0300

    added create post use case

---
+++ src/blogs/use-cases/create-post.use-case.ts
@@ -0,0 +1,31 @@
+import { v4 as uuidv4 } from 'uuid';
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { BlogsRepository } from '../blogs.repository';
+import { PostInputDto, PostViewDto } from '../../posts/posts.types';
+
+export class CreatePostCommand {
+	constructor(
+		public inputModel: PostInputDto,
+		public blogId: string,
+		public blogName: string
+	) {}
+}
+
+@CommandHandler(CreatePostCommand)
+export class CreatePostUseCase implements ICommandHandler<CreatePostCommand> {
+	constructor(private readonly blogsRepository: BlogsRepository) {}
+	async execute(command: CreatePostCommand): Promise<PostViewDto | boolean> {
+		const newPost = {
+			id: uuidv4(),
+			title: command.inputModel.title,
+			shortDescription: command.inputModel.shortDescription,
+			content: command.inputModel.content,
+			blogId: command.blogId,
+			blogName: command.blogName,
+			createdAt: new Date(),
+			likesCount: 0,
+			dislikesCount: 0,
+		};
+		return this.blogsRepository.createPostForSpecifiedBlog(newPost);
+	}
+}
