0fde40a8c8102c76e206331104789b7660c5501a
rimskayama <rimskayama@outlook.com>
2024-04-27 20:31:33 +0300

    added set comment likeStatus use case

---
+++ src/likes/use-cases/comment likes/set-comment-like-status.use-case.ts
@@ -0,0 +1,28 @@
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { v4 as uuidv4 } from 'uuid';
+import { CommentLikesRepository } from '../../comment.likes.repository';
+
+export class SetCommentLikeStatusCommand {
+	constructor(
+		public likeStatus: string,
+		public commentId: string,
+		public userId: string,
+		public userLogin: string
+	) {}
+}
+
+@CommandHandler(SetCommentLikeStatusCommand)
+export class SetCommentLikeStatusUseCase implements ICommandHandler<SetCommentLikeStatusCommand> {
+	constructor(private readonly commentLikesRepository: CommentLikesRepository) {}
+	async execute(command: SetCommentLikeStatusCommand): Promise<boolean> {
+		const like = {
+			id: uuidv4(),
+			commentId: command.commentId,
+			status: command.likeStatus,
+			userId: command.userId,
+			login: command.userLogin,
+			addedAt: new Date(),
+		};
+		return await this.commentLikesRepository.setLikeStatus(like);
+	}
+}
