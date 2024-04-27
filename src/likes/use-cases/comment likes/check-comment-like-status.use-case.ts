daaf046e71d89971d80557bf64e930f395b8dcb2
rimskayama <rimskayama@outlook.com>
2024-04-27 20:31:04 +0300

    added check comment likeStatus use case

---
+++ src/likes/use-cases/comment likes/check-comment-like-status.use-case.ts
@@ -0,0 +1,32 @@
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { CommentLikesRepository } from '../../comment.likes.repository';
+
+export class CheckCommentLikeStatusCommand {
+	constructor(
+		public likeStatus: string,
+		public commentId: string,
+		public userId: string
+	) {}
+}
+
+@CommandHandler(CheckCommentLikeStatusCommand)
+export class CheckCommentLikeStatusUseCase implements ICommandHandler<CheckCommentLikeStatusCommand> {
+	constructor(private readonly commentLikesRepository: CommentLikesRepository) {}
+	async execute(command: CheckCommentLikeStatusCommand): Promise<boolean> {
+		if (command.likeStatus === 'None') {
+			await this.commentLikesRepository.removeLike(command.commentId, command.userId);
+			return true;
+		} else {
+			//Like or Dislike
+			const likeInDB = await this.commentLikesRepository.checkLikeInDB(command.commentId, command.userId);
+			if (!likeInDB) return false; // can be created
+			if (likeInDB) {
+				if (likeInDB.status === command.likeStatus) return true;
+				if (likeInDB.status !== command.likeStatus) {
+					await this.commentLikesRepository.updateLikeStatus(command.likeStatus, command.commentId, command.userId);
+					return true;
+				}
+			}
+		}
+	}
+}
