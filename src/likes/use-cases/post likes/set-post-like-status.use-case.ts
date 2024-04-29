a1607d893b2a3a60f5afc701a97980d5f22db50c
rimskayama <rimskayama@outlook.com>
2024-04-29 08:05:50 +0300

    added set post likeStatus use case

---
+++ src/likes/use-cases/post likes/set-post-like-status.use-case.ts
@@ -0,0 +1,28 @@
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { v4 as uuidv4 } from 'uuid';
+import { PostLikesRepository } from '../../post.likes.repository';
+
+export class SetPostLikeStatusCommand {
+	constructor(
+		public likeStatus: string,
+		public postId: string,
+		public userId: string,
+		public userLogin: string
+	) {}
+}
+
+@CommandHandler(SetPostLikeStatusCommand)
+export class SetPostLikeStatusUseCase implements ICommandHandler<SetPostLikeStatusCommand> {
+	constructor(private readonly postLikesRepository: PostLikesRepository) {}
+	async execute(command: SetPostLikeStatusCommand): Promise<boolean> {
+		const like = {
+			id: uuidv4(),
+			postId: command.postId,
+			status: command.likeStatus,
+			userId: command.userId,
+			login: command.userLogin,
+			addedAt: new Date(),
+		};
+		return await this.postLikesRepository.setLikeStatus(like);
+	}
+}
