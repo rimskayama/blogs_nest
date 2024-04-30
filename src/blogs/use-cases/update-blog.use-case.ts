180c77532fa4fcab2012d91503d94047b8c95daa
rimskayama <rimskayama@outlook.com>
2024-04-30 15:46:02 +0300

    added update blog use case

---
+++ src/blogs/use-cases/update-blog.use-case.ts
@@ -0,0 +1,30 @@
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { BlogInputDto } from '../blogs.types';
+import { BlogsRepository } from '../blogs.repository';
+import { StatusCode, blogIdField, blogNotFound } from '../../exceptions/exception.constants';
+import { exceptionResultType } from '../../exceptions/exception.types';
+
+export class UpdateBlogCommand {
+	constructor(
+		public id: string,
+		public inputModel: BlogInputDto
+	) {}
+}
+
+@CommandHandler(UpdateBlogCommand)
+export class UpdateBlogUseCase implements ICommandHandler<UpdateBlogCommand> {
+	constructor(private readonly blogsRepository: BlogsRepository) {}
+	async execute(command: UpdateBlogCommand): Promise<exceptionResultType | boolean> {
+		const result = await this.blogsRepository.updateBlog(command.id, command.inputModel);
+		if (!result) {
+			return {
+				code: StatusCode.NotFound,
+				field: blogIdField,
+				message: blogNotFound,
+			};
+		}
+		return {
+			code: StatusCode.Success,
+		};
+	}
+}
