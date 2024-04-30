d28369c4aa4bd5331dc12697dce90bf362bbfc13
rimskayama <rimskayama@outlook.com>
2024-04-30 15:46:53 +0300

    added update post use case

---
+++ src/blogs/use-cases/update-post.use-case.ts
@@ -0,0 +1,43 @@
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { BlogsRepository } from '../blogs.repository';
+import { StatusCode, blogIdField, blogNotFound, postIdField, postNotFound } from '../../exceptions/exception.constants';
+import { exceptionResultType } from '../../exceptions/exception.types';
+import { BlogsQueryRepository } from '../blogs.query.repository';
+import { PostInputDto } from '../../posts/posts.types';
+
+export class UpdatePostCommand {
+	constructor(
+		public blogId: string,
+		public postId: string,
+		public inputModel: PostInputDto
+	) {}
+}
+
+@CommandHandler(UpdatePostCommand)
+export class UpdatePostUseCase implements ICommandHandler<UpdatePostCommand> {
+	constructor(
+		private readonly blogsQueryRepository: BlogsQueryRepository,
+		public readonly blogsRepository: BlogsRepository
+	) {}
+	async execute(command: UpdatePostCommand): Promise<exceptionResultType | boolean> {
+		const blog = await this.blogsQueryRepository.findBlogById(command.blogId);
+		if (!blog)
+			return {
+				code: StatusCode.NotFound,
+				message: blogNotFound,
+				field: blogIdField,
+			};
+
+		const result = await this.blogsRepository.updatePost(command.blogId, command.postId, command.inputModel);
+		if (!result) {
+			return {
+				code: StatusCode.NotFound,
+				message: postNotFound,
+				field: postIdField,
+			};
+		}
+		return {
+			code: StatusCode.Success,
+		};
+	}
+}
