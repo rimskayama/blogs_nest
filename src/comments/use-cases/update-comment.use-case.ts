aae3d8b4f4d82a85747c416c6be31e727376fb18
rimskayama <rimskayama@outlook.com>
2024-05-01 13:07:35 +0300

    added update comment use case

---
+++ src/comments/use-cases/update-comment.use-case.ts
@@ -0,0 +1,50 @@
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { StatusCode, commentIdField, commentNotFound, forbidden } from '../../exceptions/exception.constants';
+import { exceptionResultType } from '../../exceptions/exception.types';
+import { CommentsQueryRepository } from '../comments.query.repository';
+import { CommentsRepository } from '../comments.repository';
+
+export class UpdateCommentCommand {
+	constructor(
+		public commentId: string,
+		public userId: string,
+		public content: string
+	) {}
+}
+
+@CommandHandler(UpdateCommentCommand)
+export class UpdateCommentUseCase implements ICommandHandler<UpdateCommentCommand> {
+	constructor(
+		private readonly commentsQueryRepository: CommentsQueryRepository,
+		private readonly commentsRepository: CommentsRepository
+	) {}
+	async execute(command: UpdateCommentCommand): Promise<exceptionResultType | boolean> {
+		const comment = await this.commentsQueryRepository.findCommentById(command.commentId, command.userId);
+		if (!comment) {
+			return {
+				code: StatusCode.NotFound,
+				message: commentNotFound,
+				field: commentIdField,
+			};
+		}
+		if (comment.commentatorInfo.userId !== command.userId) {
+			return {
+				code: StatusCode.Forbidden,
+				message: forbidden,
+				field: commentIdField,
+			};
+		}
+
+		const result = await this.commentsRepository.updateComment(command.commentId, command.content);
+		if (!result) {
+			return {
+				code: StatusCode.NotFound,
+				message: commentNotFound,
+				field: commentIdField,
+			};
+		}
+		return {
+			code: StatusCode.Success,
+		};
+	}
+}
