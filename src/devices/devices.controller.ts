4590bb39a2624a37f72a3beab2fcbf0cdbed6943
rimskayama <rimskayama@outlook.com>
2024-05-02 11:07:35 +0300

    added use cases, devices refactoring

--- src/devices/devices.controller.ts
+++ src/devices/devices.controller.ts
@@ -1,33 +1,26 @@
-import {
-	Controller,
-	Delete,
-	Get,
-	HttpCode,
-	HttpStatus,
-	Param,
-	Request,
-	UnauthorizedException,
-	UseGuards,
-} from '@nestjs/common';
-import { DevicesService } from './devices.service';
+import { Controller, Delete, Get, HttpCode, HttpStatus, Param, UnauthorizedException, UseGuards } from '@nestjs/common';
 import { DevicesQueryRepository } from './devices.query.repository';
 import { exceptionHandler } from '../exceptions/exception.handler';
-import { StatusCode, deviceIdField, deviceNotFound, forbidden } from '../exceptions/exception.constants';
+import { StatusCode } from '../exceptions/exception.constants';
 import { JwtRefreshGuard } from '../auth/passport/guards/jwt-refresh.guard';
 import { UserFromReq } from '../auth/decorators/userId.decorator';
 import { UserFromGuard } from '../users/users.types';
+import { DeviceIdFromReq } from '../auth/decorators/deviceId.decorator';
+import { CommandBus } from '@nestjs/cqrs';
+import { TerminateAllSessionsCommand } from './use-cases/terminate-all-sessions.use-case';
+import { TerminateSessionCommand } from './use-cases/terminate-session.use-case';
 
 @Controller('security/devices')
 export class DevicesController {
 	constructor(
-		private readonly devicesService: DevicesService,
+		private commandBus: CommandBus,
 		private readonly devicesQueryRepository: DevicesQueryRepository
 	) {}
 
 	@UseGuards(JwtRefreshGuard)
 	@Get()
 	@HttpCode(HttpStatus.OK)
-	async getDevices(@Request() req, @UserFromReq() user: UserFromGuard) {
+	async getDevices(@UserFromReq() user: UserFromGuard) {
 		const result = await this.devicesQueryRepository.findDevices(user.id);
 		return result;
 	}
@@ -35,39 +28,22 @@ export class DevicesController {
 	@UseGuards(JwtRefreshGuard)
 	@Delete()
 	@HttpCode(HttpStatus.NO_CONTENT)
-	async deleteAllOthersSessions(@Request() req) {
-		const deviceIdFromRT = req.user.deviceId;
-		const session = await this.devicesQueryRepository.findDeviceByDeviceId(deviceIdFromRT);
+	async deleteAllOtherSessions(@DeviceIdFromReq() deviceId: string) {
+		const session = await this.devicesQueryRepository.findDeviceByDeviceId(deviceId);
 
 		if (session) {
-			return await this.devicesService.terminateAllSessions(session.userId, session.deviceId);
+			return await this.commandBus.execute(new TerminateAllSessionsCommand(session.userId, session.deviceId));
 		} else throw new UnauthorizedException();
 	}
 
 	@UseGuards(JwtRefreshGuard)
 	@Delete(':id')
 	@HttpCode(HttpStatus.NO_CONTENT)
-	async deleteSession(@Request() req, @Param('id') deviceIdFromReq: string) {
-		//Req
-		if (!deviceIdFromReq) {
-			return exceptionHandler(StatusCode.NotFound, deviceNotFound, deviceIdField);
+	async deleteSession(@Param('id') deviceIdFromReq: string, @DeviceIdFromReq() deviceIdFromGuard: string) {
+		const result = await this.commandBus.execute(new TerminateSessionCommand(deviceIdFromReq, deviceIdFromGuard));
+		if (result.code !== StatusCode.Success) {
+			return exceptionHandler(result.code, result.message, result.field);
 		}
-		const userFromReq = await this.devicesQueryRepository.findDeviceByDeviceId(deviceIdFromReq);
-		if (!userFromReq) {
-			return exceptionHandler(StatusCode.NotFound, deviceNotFound, deviceIdField);
-		}
-
-		//RT
-		const deviceIdFromRT = req.user.deviceId;
-		const userFromRT = await this.devicesQueryRepository.findDeviceByDeviceId(deviceIdFromRT);
-		if (!userFromRT) {
-			return exceptionHandler(StatusCode.NotFound, deviceNotFound, deviceIdField);
-		}
-
-		if (userFromReq.userId !== userFromRT.userId) {
-			return exceptionHandler(StatusCode.Forbidden, forbidden, deviceIdField);
-		} else {
-			return await this.devicesService.terminateSession(deviceIdFromReq);
-		}
+		return result;
 	}
 }
--- src/devices/devices.module.ts
+++ src/devices/devices.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DevicesController } from './devices.controller';
-import { DevicesService } from './devices.service';
 import { DevicesRepository } from './devices.repository';
 import { DevicesQueryRepository } from './devices.query.repository';
 import { PassportModule } from '@nestjs/passport';
@@ -11,11 +10,21 @@ import { RefreshTokenValidationUseCase } from '../auth/use-cases/validations/val
 import { CqrsModule } from '@nestjs/cqrs';
 import { AccessTokenValidationUseCase } from '../auth/use-cases/validations/validate-access-token.use-case';
 import { CreateDeviceUseCase } from './use-cases/create-device.use-case';
+import { UpdateDeviceUseCase } from './use-cases/update-device-tokens.use-case';
+import { TerminateAllSessionsUseCase } from './use-cases/terminate-all-sessions.use-case';
+import { TerminateSessionUseCase } from './use-cases/terminate-session.use-case';
 
 const strategies = [JwtRefreshTokenStrategy];
-const services = [JwtService, DevicesService];
+const services = [JwtService];
 const adapters = [DevicesRepository, DevicesQueryRepository];
-const useCases = [RefreshTokenValidationUseCase, AccessTokenValidationUseCase, CreateDeviceUseCase];
+const useCases = [
+	RefreshTokenValidationUseCase,
+	AccessTokenValidationUseCase,
+	CreateDeviceUseCase,
+	UpdateDeviceUseCase,
+	TerminateAllSessionsUseCase,
+	TerminateSessionUseCase,
+];
 
 @Module({
 	imports: [ConfigModule.forRoot(), PassportModule, CqrsModule],
