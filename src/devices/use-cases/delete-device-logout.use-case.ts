b77805124a10a2d50017f92ee0b6b41d3708128a
rimskayama <rimskayama@outlook.com>
2024-05-02 11:04:48 +0300

    added device logout use case

---
+++ src/devices/use-cases/delete-device-logout.use-case.ts
@@ -0,0 +1,15 @@
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { DevicesRepository } from '../devices.repository';
+import { exceptionResultType } from '../../exceptions/exception.types';
+
+export class DeviceLogoutCommand {
+	constructor(public deviceId: string) {}
+}
+
+@CommandHandler(DeviceLogoutCommand)
+export class DeviceLogoutUseCase implements ICommandHandler<DeviceLogoutCommand> {
+	constructor(private readonly devicesRepository: DevicesRepository) {}
+	async execute(command: DeviceLogoutCommand): Promise<exceptionResultType | boolean> {
+		return await this.devicesRepository.terminateSession(command.deviceId);
+	}
+}
