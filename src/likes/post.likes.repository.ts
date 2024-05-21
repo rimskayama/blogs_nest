2747f700efc89629eef70aaa6e8855426dbc827e
rimskayama <rimskayama@outlook.com>
2024-05-21 12:58:49 +0300

    post likes typeOrm migration

--- src/likes/post.likes.repository.ts
+++ src/likes/post.likes.repository.ts
@@ -1,57 +1,16 @@
 import { Injectable } from '@nestjs/common';
-import { InjectDataSource } from '@nestjs/typeorm';
-import { DataSource } from 'typeorm';
-import { LikeStatus, PostLikeType } from './likes.types';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { PostLike } from './post-like.entity';
+import { PostLikeType } from './likes.types';
 
 @Injectable()
 export class PostLikesRepository {
-	constructor(@InjectDataSource() protected dataSource: DataSource) {}
-
-	async countLikes(postId: string) {
-		let likes;
-		let dislikes;
-		const queryToCountLikes = `
-		SELECT count(*) as total
-		FROM public."PostLikes" pl
-		WHERE pl."postId" = $1 AND pl."status" = $2;`;
-
-		try {
-			likes = await this.dataSource.query(queryToCountLikes, [postId, LikeStatus.Like]);
-			dislikes = await this.dataSource.query(queryToCountLikes, [postId, LikeStatus.Dislike]);
-		} catch (error) {
-			console.error('Error counting post likes', error);
-		}
-
-		const likesCount = parseInt(likes[0].total);
-		const dislikesCount = parseInt(dislikes[0].total);
-
-		return {
-			likesCount: likesCount,
-			dislikesCount: dislikesCount,
-		};
-	}
+	constructor(@InjectRepository(PostLike) private readonly postLikesRepository: Repository<PostLike>) {}
 
 	async setLikeStatus(newStatus: PostLikeType) {
-		const query = `INSERT INTO public."PostLikes"(
-				"id", "postId", "status", "userId", "login", "addedAt")
-			VALUES($1, $2, $3, $4, $5, $6)
-			RETURNING *
-			`;
-
-		const values = [
-			newStatus.id,
-			newStatus.postId,
-			newStatus.status,
-			newStatus.userId,
-			newStatus.login,
-			newStatus.addedAt,
-		];
-
 		try {
-			const result = await this.dataSource.query(query, values);
-			if (result[1] === 0) {
-				return false;
-			}
+			await this.postLikesRepository.save(newStatus);
 			return true;
 		} catch (error) {
 			console.error('Error creating post like status :', error);
@@ -60,63 +19,57 @@ export class PostLikesRepository {
 	}
 
 	async checkLikeInDB(postId: string, userId: string) {
-		const query = `
-        SELECT "status"
-		FROM public."PostLikes" pl
-		WHERE pl."postId" = $1 AND pl."userId" = $2;
-    `;
-
 		try {
-			const result = await this.dataSource.query(query, [postId, userId]);
-			return result[0];
+			const result = await this.postLikesRepository
+				.createQueryBuilder('pl')
+				.select(['pl.status'])
+				.where(`pl.postId = :postId`, {
+					postId: postId,
+				})
+				.andWhere(`pl.userId = :userId`, {
+					userId: userId,
+				})
+				.getOne();
+			return result;
 		} catch (error) {
-			console.error('Error finding post like:', error);
+			console.error('Error finding post like status:', error);
 			return null;
 		}
 	}
 
 	async updateLikeStatus(likeStatus: string, postId: string, userId: string) {
-		const query = `
-			UPDATE public."PostLikes" pl
-			SET "status"=$1, "addedAt"=$2
-			WHERE pl."postId" = $3 AND pl."userId" = $4;
-		`;
-		const values = [likeStatus, new Date(), postId, userId];
-
 		try {
-			return await this.dataSource.query(query, values);
+			const like = await this.postLikesRepository
+				.createQueryBuilder('pl')
+				.where('pl.postId = :postId', {
+					postId: postId,
+				})
+				.andWhere('pl.userId = :userId', {
+					userId: userId,
+				})
+				.getOne();
+			like.status = likeStatus;
+			like.addedAt = new Date();
+			await this.postLikesRepository.save(like);
+			return;
 		} catch (error) {
 			console.error('Error updating post like status:', error);
 		}
 	}
 
 	async removeLike(postId: string, userId: string) {
-		const query = `
-		DELETE FROM public."PostLikes" pl
-        WHERE pl."postId" = $1 AND pl."userId" = $2;
-	`;
 		try {
-			const result = await this.dataSource.query(query, [postId, userId]);
-			if (result[1] === 0) {
-				return null;
-			}
-			return true;
+			const result = await this.postLikesRepository
+				.createQueryBuilder('pl')
+				.delete()
+				.from(PostLike)
+				.where('postId = :postId', { postId: postId })
+				.andWhere('userId = :userId', { userId: userId })
+				.execute();
+			return result.affected === 1;
 		} catch (error) {
 			console.error('Error deleting post like status:', error);
 			return null;
-		}
-	}
-	async updatePostLikes(postId: string, likesCount: number, dislikesCount: number) {
-		const query = `
-		UPDATE public."Posts" p
-		SET "likesCount"=$1, "dislikesCount"=$2
-		WHERE p."id" = $3;
-	`;
-		try {
-			await this.dataSource.query(query, [likesCount, dislikesCount, postId]);
-			return true;
-		} catch (error) {
-			console.error('Error updating post likes:', error);
 		}
 	}
 }
--- src/posts/posts.query.repository.ts
+++ src/posts/posts.query.repository.ts
@@ -1,12 +1,17 @@
 import { PostViewDto, postsPaginationDto } from './posts.types';
-import { Post, likeDetails } from './post.entity';
+import { Post } from './post.entity';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { PostLike } from '../likes/post-like.entity';
+import { LikeStatus } from '../likes/likes.types';
 
 @Injectable()
 export class PostsQueryRepository {
-	constructor(@InjectRepository(Post) private readonly postsRepository: Repository<Post>) {}
+	constructor(
+		@InjectRepository(Post) private readonly postsRepository: Repository<Post>,
+		@InjectRepository(PostLike) private readonly postLikesRepository: Repository<PostLike>
+	) {}
 
 	async findPosts(
 		pageNumber: number,
@@ -35,15 +40,69 @@ export class PostsQueryRepository {
 
 		const items = await Promise.all(
 			result.map(async (post) => {
-				const likeStatus = 'None';
-				let newestLikes: [];
+				let likeInfo;
+				let likeStatus = 'None';
+				let likesCount = 0;
+				let dislikesCount = 0;
+				let newestLikes = [];
 				const blogName = post.blog.name;
+
+				//set like
+				if (userId) {
+					likeInfo = await this.postLikesRepository
+						.createQueryBuilder('pl')
+						.select(['pl.id', 'pl.postId', 'pl.status', 'pl.userId'])
+						.where(`pl.postId = :postId`, {
+							postId: post.id,
+						})
+						.andWhere(`pl.userId = :userId`, {
+							userId: userId,
+						})
+						.getOne();
+					if (likeInfo) {
+						likeStatus = likeInfo.status;
+					}
+				}
+				likesCount = await this.postLikesRepository
+					.createQueryBuilder('pl')
+					.where(`pl.postId = :id`, {
+						id: post.id,
+					})
+					.andWhere(`pl.status = :status`, {
+						status: LikeStatus.Like,
+					})
+					.getCount();
+
+				dislikesCount = await this.postLikesRepository
+					.createQueryBuilder('pl')
+					.where(`pl.postId = :id`, {
+						id: post.id,
+					})
+					.andWhere(`pl.status = :status`, {
+						status: LikeStatus.Dislike,
+					})
+					.getCount();
+
+				newestLikes = await this.postLikesRepository
+					.createQueryBuilder('pl')
+					.leftJoin('pl.user', 'u')
+					.select(['pl.id', 'pl.addedAt', 'pl.userId', 'u.login'])
+					.where(`pl.postId = :postId`, {
+						postId: post.id,
+					})
+					.andWhere(`pl.status = :status`, {
+						status: LikeStatus.Like,
+					})
+					.orderBy(`pl.addedAt`, 'DESC')
+					.skip(0)
+					.take(3)
+					.getMany();
 
 				return Post.getViewPost({
 					...post,
 					blogName,
-					likesCount: 0,
-					dislikesCount: 0,
+					likesCount,
+					dislikesCount,
 					myStatus: likeStatus,
 					newestLikes,
 				});
@@ -59,8 +118,10 @@ export class PostsQueryRepository {
 	}
 
 	async findPostById(postId: string, userId: string | false): Promise<PostViewDto | null> {
+		let result;
+		let blogName;
 		try {
-			const result = await this.postsRepository
+			result = await this.postsRepository
 				.createQueryBuilder('p')
 				.leftJoin('p.blog', 'b')
 				.select(['p.id', 'p.title', 'p.shortDescription', 'p.content', 'p.blogId', 'p.createdAt', 'b.name'])
@@ -68,23 +129,78 @@ export class PostsQueryRepository {
 					postId: postId,
 				})
 				.getOne();
-
-			const likeStatus = 'None';
-			let newestLikes: [];
-			const blogName = result.blog.name;
-
-			return Post.getViewPost({
-				...result,
-				blogName,
-				likesCount: 0,
-				dislikesCount: 0,
-				myStatus: likeStatus,
-				newestLikes,
-			});
+			blogName = result.blog.name;
 		} catch (error) {
 			console.error('Error finding post:', error);
 			return null;
 		}
+
+		let likeInfo;
+		let likeStatus = 'None';
+		let likesCount = 0;
+		let dislikesCount = 0;
+		let newestLikes = [];
+
+		//set like
+		if (userId) {
+			likeInfo = await this.postLikesRepository
+				.createQueryBuilder('pl')
+				.select(['pl.id', 'pl.postId', 'pl.status', 'pl.userId'])
+				.where(`pl.postId = :postId`, {
+					postId: result.id,
+				})
+				.andWhere(`pl.userId = :userId`, {
+					userId: userId,
+				})
+				.getOne();
+			if (likeInfo) {
+				likeStatus = likeInfo.status;
+			}
+		}
+
+		likesCount = await this.postLikesRepository
+			.createQueryBuilder('pl')
+			.where(`pl.postId = :postId`, {
+				postId: result.id,
+			})
+			.andWhere(`pl.status = :status`, {
+				status: LikeStatus.Like,
+			})
+			.getCount();
+
+		dislikesCount = await this.postLikesRepository
+			.createQueryBuilder('pl')
+			.where(`pl.postId = :postId`, {
+				postId: result.id,
+			})
+			.andWhere(`pl.status = :status`, {
+				status: LikeStatus.Dislike,
+			})
+			.getCount();
+
+		newestLikes = await this.postLikesRepository
+			.createQueryBuilder('pl')
+			.leftJoin('pl.user', 'u')
+			.select(['pl.id', 'pl.addedAt', 'pl.userId', 'u.login'])
+			.where(`pl.postId = :postId`, {
+				postId: result.id,
+			})
+			.andWhere(`pl.status = :status`, {
+				status: LikeStatus.Like,
+			})
+			.orderBy(`pl.addedAt`, 'DESC')
+			.skip(0)
+			.take(3)
+			.getMany();
+
+		return Post.getViewPost({
+			...result,
+			blogName,
+			likesCount,
+			dislikesCount,
+			myStatus: likeStatus,
+			newestLikes,
+		});
 	}
 
 	async findPostsByBlogId(
@@ -109,6 +225,9 @@ export class PostsQueryRepository {
 
 		const count = await this.postsRepository
 			.createQueryBuilder('p')
+			.where(`p.blogId = :blogId`, {
+				blogId: blogId,
+			})
 			.orderBy(`p.${sortBy}`, sortDirection)
 			.skip((pageNumber - 1) * pageSize)
 			.take(pageSize)
@@ -118,15 +237,69 @@ export class PostsQueryRepository {
 
 		const items = await Promise.all(
 			result.map(async (post) => {
-				const likeStatus = 'None';
+				let likeInfo;
+				let likeStatus = 'None';
+				let likesCount = 0;
+				let dislikesCount = 0;
 				const blogName = post.blog.name;
-				const newestLikes: likeDetails[] = [];
+				let newestLikes = [];
+
+				//set like
+				if (userId) {
+					likeInfo = await this.postLikesRepository
+						.createQueryBuilder('pl')
+						.select(['pl.id', 'pl.postId', 'pl.status', 'pl.userId'])
+						.where(`pl.postId = :postId`, {
+							postId: post.id,
+						})
+						.andWhere(`pl.userId = :userId`, {
+							userId: userId,
+						})
+						.getOne();
+					if (likeInfo) {
+						likeStatus = likeInfo.status;
+					}
+				}
+				likesCount = await this.postLikesRepository
+					.createQueryBuilder('pl')
+					.where(`pl.postId = :id`, {
+						id: post.id,
+					})
+					.andWhere(`pl.status = :status`, {
+						status: LikeStatus.Like,
+					})
+					.getCount();
+
+				dislikesCount = await this.postLikesRepository
+					.createQueryBuilder('pl')
+					.where(`pl.postId = :id`, {
+						id: post.id,
+					})
+					.andWhere(`pl.status = :status`, {
+						status: LikeStatus.Dislike,
+					})
+					.getCount();
+
+				newestLikes = await this.postLikesRepository
+					.createQueryBuilder('pl')
+					.leftJoin('pl.user', 'u')
+					.select(['pl.id', 'pl.addedAt', 'pl.userId', 'u.login'])
+					.where(`pl.postId = :id`, {
+						id: post.id,
+					})
+					.andWhere(`pl.status = :status`, {
+						status: LikeStatus.Like,
+					})
+					.orderBy(`pl.addedAt`, 'DESC')
+					.skip(0)
+					.take(3)
+					.getMany();
 
 				return Post.getViewPost({
 					...post,
 					blogName,
-					likesCount: 0,
-					dislikesCount: 0,
+					likesCount,
+					dislikesCount,
 					myStatus: likeStatus,
 					newestLikes,
 				});
