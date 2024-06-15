306d658ea0d153696c98fa89ca8195124102c924
rimskayama <rimskayama@outlook.com>
2024-06-15 16:00:38 +0300

    fixing

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { appSettings } from './app.settings';
 
-export async function bootstrap() {
+export default async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	appSettings(app);
 	await app.listen(process.env.PORT || 5000);
