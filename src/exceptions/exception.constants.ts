02c0005897cc7e9dce47343a67a6667fb7f60b1b
rimskayama <rimskayama@outlook.com>
2024-08-26 22:29:25 +0300

    added question constants

--- src/exceptions/exception.constants.ts
+++ src/exceptions/exception.constants.ts
@@ -4,6 +4,7 @@ export const postIdField = 'postId';
 export const userIdField = 'userId';
 export const commentIdField = 'commentId';
 export const deviceIdField = 'deviceId';
+export const questionIdField = 'questionId';
 
 // Not found
 export const blogNotFound = 'Blog not found';
@@ -11,6 +12,7 @@ export const postNotFound = 'Post not found';
 export const userNotFound = 'User not found';
 export const commentNotFound = 'Comment not found';
 export const deviceNotFound = 'Device not found';
+export const questionNotFound = 'Question not found';
 
 // Forbidden
 
